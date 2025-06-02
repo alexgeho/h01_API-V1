b5f2066eeb154367f4d1eb3794cbfc0761826fba
alexgeho <870717ag@gmail.com>
2025-06-02 18:59:51 +0200

    Полный коммит всех файлов проекта

---
+++ Desktop/1 it-incubator/h01_API-V1/src/setup-app.ts
@@ -0,0 +1,18 @@
+import express, { Express, Request, Response } from 'express';
+import { driversRouter } from './drivers/routers/drivers.router';
+import { testingRouter } from './testing/routers/testing.router';
+import { setupSwagger } from './core/swagger/setup-swagger';
+
+export const setupApp = (app: Express) => {
+  app.use(express.json());
+
+  app.get('/', (req: Request, res: Response) => {
+    res.status(200).send('hello world Bitau!!!');
+  });
+
+  app.use('/api/drivers', driversRouter);
+  app.use('/api/testing', testingRouter);
+
+  setupSwagger(app);
+  return app;
+};
